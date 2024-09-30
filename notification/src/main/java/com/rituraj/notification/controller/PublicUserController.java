1525b144ec245a2d47b7aa1a43b437ae2ad3ce2b
Rituraj Seal <rituraj2001.rs@gmail.com>
2024-09-30 19:22:27 +0530

    Added spring security to some routes

--- notification/src/main/java/com/rituraj/notification/controller/PublicUserController.java
+++ notification/src/main/java/com/rituraj/notification/controller/PublicUserController.java
@@ -1,30 +1,29 @@
 package com.rituraj.notification.controller;
 
 import com.rituraj.notification.entity.User;
-import com.rituraj.notification.repository.UserRepository;
 import com.rituraj.notification.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController()
-@RequestMapping("/protected/user")
-public class UserController {
+public class PublicUserController {
 
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public PublicUserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<?> getAllUsers(){
-        return ResponseEntity.ok().body(userService.getAllUsers());
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user){
+        if( userService.saveUser(user) ){
+            return ResponseEntity.ok().body("User created successfully!");
+        }
+        else{
+            return ResponseEntity.internalServerError().body("Something went wrong!");
+        }
     }
-//    @PostMapping("/verify")
-//    public ResponseEntity<?> verifyUserMail(@Valid @RequestBody User user){}
 }
